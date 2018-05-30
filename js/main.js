3cfe14546deff1d0fb0bf11006f5f8e87acc029f
yersultanzada <yersultan.zada@gmail.com>
2018-05-30 17:38:51 +0600

    ...

--- js/main.js
+++ js/main.js
@@ -1,31 +1,5 @@
 $(document).ready(function () {
 
-    $("#main-carousel").owlCarousel({
-        loop: true,
-        items: 1,
-        autoplay: true,
-        autoplayTimeout: 3000,
-        autoplayHoverPause: true,
-        video:true,
-        center: true,
-        nav: true,
-        navText: [
-            "<i class='mdi mdi-chevron-left'></i>",
-            "<i class='mdi mdi-chevron-right'></i>"
-        ]
-    });
-
-    $(".dropdown-button").dropdown({
-        hover: true,
-        coverTrigger: false
-    });
-
-    $('.sidenav').sidenav();
-
-    var dropdownMenu = $(".dropdown-content li a");
-    dropdownMenu.click(function () {
-            dropdownMenu.removeClass('active');
-            $(this).addClass('active')
-    });
+    $('.collapsible').collapsible();
 
 });
