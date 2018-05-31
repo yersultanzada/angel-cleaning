f7bb5e0511e16dc457c31228c9aea1275ea5b55b
yersultanzada <yersultan.zada@gmail.com>
2018-05-31 17:37:19 +0600

    some changes

--- js/main.js
+++ js/main.js
@@ -2,4 +2,12 @@ $(document).ready(function () {
 
     $('.collapsible').collapsible();
 
+    /**
+     *  Маска для телефона
+     */
+
+    jQuery(function($){
+        $("#tel").mask("+7(999)999-99-99");
+    });
+
 });
