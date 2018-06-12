b2b09b8268fb7b120afb410c6182b691c64868b9
yersultanzada <yersultan.zada@gmail.com>
2018-06-12 17:33:56 +0600

    last block

--- js/main.js
+++ js/main.js
@@ -8,4 +8,20 @@ $(document).ready(function () {
         $("#tel2").mask("+7(999)999-99-99");
     });
 
+    $('._dot img').hover(function () {
+        $('._dot img').css({
+            'cursor': 'pointer'
+        });
+        $('._dot .shown').css({
+            'display': 'none'
+        });
+        $('._dot .hidden').css({
+            'display': 'inline-block'
+        });
+    });
+
+    $(function () {
+        $('[data-toggle="popover"]').popover()
+    })
+
 });
